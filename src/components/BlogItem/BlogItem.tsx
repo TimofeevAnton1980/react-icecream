1aea69b005fe076e34cfc7d3b3df71b0e76c67c7
antontimofeev <Maestroat@yandex.ru>
2023-04-15 20:41:20 +0300

    36_good_end_node18_no calendar

--- src/components/BlogItem/BlogItem.tsx
+++ src/components/BlogItem/BlogItem.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import * as styles from './styles.module.css';
 import { imgSize } from '../constants/heigh-width';
 import { apelsinImages } from '../constants/imgApelsin';
-import { ImCalendar } from 'react-icons/Im';
+// import { ImCalendar } from 'react-icons/Im';
 import { Link } from 'gatsby';
 
 interface ItemInterface {
@@ -19,7 +19,7 @@ export const BlogItem: React.FC<ItemInterface>  = ({ style, blogName, blogImg, b
     return(
         <div className={styles.item}  style={{ ...style }}>
             <div className={styles.dateWrap}>
-            <span><ImCalendar /> </span>
+            {/*<span><ImCalendar /> </span>*/}
             <span>Опубликовано: {blogDate}</span>
             </div>
                 <h2 className={styles.title}><Link to={`/${blogPage}`} state={{ mode: "blog", blogDate: `${blogDate}`, blogName: `${blogName}`}} title="Новость подробнее">{blogName}</Link></h2>
