6ba56e3e376f15cf2ae7fc6151bc42ab53abdf65
antontimofeev <Maestroat@yandex.ru>
2023-05-21 13:03:00 +0300

    36_good_end_new_9

--- src/hooks/innerHeight.ts
+++ src/hooks/innerHeight.ts
@@ -1,15 +1,27 @@
-export const useInnerHeight = () => {
-    const isBrowser = typeof window !== 'undefined';
-    if (isBrowser) {
-        (function init100vh() {
-            function setHeight() {
-                const vh = window.innerHeight * 0.01;
-                document.documentElement.style.setProperty('--vh', `${vh}px`);
-                // console.log(vh * 100);
-            }
+import { useWindowSize } from './useWindowSize';
 
-            setHeight();
-            window.addEventListener('resize', setHeight);
-        })();
+// export const useInnerHeight = () => {
+//     const isBrowser = typeof window !== 'undefined';
+//     if (isBrowser) {
+//         (function init100vh() {
+//             function setHeight() {
+//                 const vh = window.innerHeight * 0.01;
+//                 document.documentElement.style.setProperty('--vh', `${vh}px`);
+//                 // console.log(vh * 100);
+//             }
+//
+//             setHeight();
+//             window.addEventListener('resize', setHeight);
+//         })();
+//     }
+// };
+
+export const useInnerHeight = () => {
+    const [, , height] = useWindowSize();
+
+    function setScreen() {
+        const vh = height * 0.01;
+        document.documentElement.style.setProperty('--vh', `${vh}px`);
     }
+    setScreen();
 };
--- src/hooks/useWindowSize.ts
+++ src/hooks/useWindowSize.ts
@@ -1,4 +1,4 @@
-import React, { useState, useLayoutEffect } from 'react';
+import { useState, useLayoutEffect } from 'react';
 
 export const useWindowSize = function () {
     const [width, setWidth] = useState(0);
